8495a9452400bff6743ae1de8365c704e9bf8186
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-12-25 00:45:10 +0000

    Update 2 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
+      <div className="bg-gray-900 p-9">
+        <ul className="block">
+          <li className="text-white font-bold inline m-8 hover:text-gray-600">
+            <Link href="/">Welcome</Link>
+          </li>
+          <li className="text-white font-bold inline hover:text-gray-600">
+            <Link href="/pages/about">About</Link>
+          </li>
+        </ul>
+      </div>
       <div className="bg-gray-800 p-9 text-center">
         <p className="text-white font-bold font-sans text-3xl">
           Techtack-Technologies
@@ -18,9 +30,18 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-gray-500 p-6">
+      <div className="bg-gray-800 p-6">
         <p className="text-white font-mono text-center">
           &copy; Copyright 2023 Techtack-Technologies. All right reserved.
+        </p>
+      </div>
+      <div className="bg-gray-900 p-5">
+        <p className="text-white font-bold font-sans text-center">
+          Help us improve our website by sending feedback about your usage of
+          our website.
+          <a className="text-blue-600 hover:text-blue-400" href="#">
+            Send FeedBack!
+          </a>
         </p>
       </div>
     </div>
---
+++ app/pages/about.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div>
+      <div className="bg-gray-800 p-9 text-center">
+        <p className="text-white font-bold font-sans text-3xl">
+          Techtack-Technologies | About
+        </p>
+      </div>
+      <div className="bg-gray-800 p-6">
+        <p className="text-white font-mono text-center">
+          &copy; Copyright 2023 Techtack-Technologies. All right reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
