5e8ed2e7539ebcdf64f66ef07baf323755a1765d
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-12-25 14:08:43 +0000

    Create about.tsx and portal.tsx

---
+++ app/src/pages/portal.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Portal() {
+  return (
+    <div>
+      <div className="bg-gray-800 p-9">
+        <h3 className="text-white font-bold font-sans text-center">
+          portal page
+        </h3>
+      </div>
+    </div>
+  );
+}
